31719669353a6b93466636453f2a7c7c5e672048
Dennis0101 <67768185+Dennis0101@users.noreply.github.com>
2025-07-16 19:09:55 +0900

    Create _app.js

---
+++ pages/_app.js
@@ -0,0 +1,10 @@
+import '../styles/globals.css'
+import { ThemeProvider } from '../components/ThemeProvider'
+
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <ThemeProvider>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
+}
