1a1917391a5fe01e3b17fc3ebd71e7b39fd0517d
Dennis0101 <67768185+Dennis0101@users.noreply.github.com>
2025-07-16 19:20:00 +0900

    Create products.js

---
+++ pages/api/products.js
@@ -0,0 +1,23 @@
+import { db } from '../../lib/firebaseAdmin'
+
+export default async function handler(req, res) {
+  const col = db.collection('products')
+  if (req.method === 'GET') {
+    const snap = await col.get()
+    const list = snap.docs.map(d => ({ id: d.id, ...d.data() }))
+    return res.json(list)
+  }
+  if (req.method === 'POST') {
+    const data = req.body
+    await col.add({
+      ...data,
+      price: Number(data.price),
+      stock: Number(data.stock),
+      createdAt: Date.now()
+    })
+    const snap = await col.get()
+    const list = snap.docs.map(d => ({ id: d.id, ...d.data() }))
+    return res.json(list)
+  }
+  res.status(405).end()
+}
