c769e70f98b4a8f020b62542d1c76dbfbf073775
Dennis0101 <67768185+Dennis0101@users.noreply.github.com>
2025-07-19 15:10:17 +0900

    Update ThemeProvider.jsx

--- components/ThemeProvider.jsx
+++ components/ThemeProvider.jsx
@@ -1,15 +1,23 @@
-import { useState, createContext } from 'react'
+import { createContext, useState, useEffect } from 'react'
 
 export const ThemeContext = createContext()
 
 export function ThemeProvider({ children }) {
   const [dark, setDark] = useState(true)
 
+  useEffect(() => {
+    if (dark) {
+      document.documentElement.classList.add('dark')
+    } else {
+      document.documentElement.classList.remove('dark')
+    }
+  }, [dark])
+
   const toggleTheme = () => setDark(prev => !prev)
 
   return (
     <ThemeContext.Provider value={{ dark, toggleTheme }}>
-      <div className={dark ? 'dark' : ''}>{children}</div>
+      {children}
     </ThemeContext.Provider>
   )
 }
