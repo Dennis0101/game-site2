65c9ec5cf65e2a9289dfeb0223887627bb3eb62c
Dennis0101 <67768185+Dennis0101@users.noreply.github.com>
2025-07-19 15:14:18 +0900

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,12 @@
 import '../styles/globals.css'
 import { ThemeProvider } from '../components/ThemeProvider'
+import DarkModeToggle from '../components/DarkModeToggle'
 
 export default function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider>
       <Component {...pageProps} />
+      <DarkModeToggle />
     </ThemeProvider>
   )
 }
